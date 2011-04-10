7c551d343820aff5308f19bdf1b3deeb940bd953
Adam Miribyan <kreditmarket.com@gmail.com>
2011-04-10 07:47:39 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+gem 'mysql2'
+
+gem 'haml'
+gem 'haml-rails'
+gem 'less'
+gem 'simple_form'
+gem 'russian'
+gem 'jquery-rails', '~>0.2'
+gem 'capistrano'
+
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuam::Application.config.secret_token = '799e80fd7cdf5ad977c7f12b82739007214374c08c531a77d3bb776b666a8a1ca6671b98a0b7b2e547f04c271842d88ad7817eb8eef5cd4de4d255bc44e297cc'

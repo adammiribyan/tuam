2e79764e2c53c6e6409bb7870a7a68e7e765ebd7
Adam Miribyan <kreditmarket.com@gmail.com>
2011-04-13 16:15:40 +0400

    improved search method

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ActiveRecord::Base
   def self.search(query)
     if query
-      find(:all, :conditions => ['full_name LIKE ?', "%#{query}%"], :limit => 1)
+      find(:conditions => ['full_name LIKE ?', "%#{query}%"])
     else
       nil
     end

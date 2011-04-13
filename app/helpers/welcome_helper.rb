4742c3d4cc175a0e644c0ce8dc0bfc9b3dba139f
Adam Miribyan <kreditmarket.com@gmail.com>
2011-04-13 15:59:19 +0400

    show the date of birth if exists

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,11 +2,14 @@
 
 module WelcomeHelper
   def answer
-    if @client.present?
-      "— <span class=\"ano\">Да.</span>".html_safe
+    if @client.present?     
+      if @client.birth_date.present?
+        "— <span class=\"ano\">Да.</span><br />— <span>Дата рождения: #{@client.birth_date}.</span>".html_safe
+      else
+        "— <span class=\"ano\">Да.</span>".html_safe
+      end
     else
       "— <span class=\"ne\">Нет.</span>".html_safe
     end
-  end
-  
+  end  
 end

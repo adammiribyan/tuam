7534ad721cafb02ad9c5b3d952f9042d37436846
Adam <adam.miribyan@gmail.com>
2011-04-23 04:42:11 -0700

    removed clients table migration file

--- db/migrate/20110410050720_create_clients.rb
+++ db/migrate/20110410050720_create_clients.rb
@@ -1,11 +1,7 @@
 class CreateClients < ActiveRecord::Migration
   def self.up
-    create_table :clients do |t|
-      t.string :full_name      
-    end
   end
 
   def self.down
-    drop_table :clients
   end
 end
